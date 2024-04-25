3304d36214e7a7fa86262acaab2d92989a4338fc
nenekiri@doombot <ryota.jack.825@gmail.com>
2024-04-25 23:53:22 +0900

    add image

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <img alt="Vue logo" src="./assets/snow_leopard.jpg">
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <TestPages msg="Welcome to Your Vue.js App"/>
 </template>
